27d4df3a46c140f455c41cbdae66277c9d02a08a
yikuzojojo <1724078638@qq.com>
2025-04-21 17:13:13 +0800

    3.图片模块

--- src/main/java/com/jojo/jobapicturebackend/JobaPictureBackendApplication.java
+++ src/main/java/com/jojo/jobapicturebackend/JobaPictureBackendApplication.java
@@ -1,10 +1,12 @@
 package com.jojo.jobapicturebackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com.jojo.jobapicturebackend.mapper")
 @EnableAspectJAutoProxy(exposeProxy = true)
 public class JobaPictureBackendApplication {
 
