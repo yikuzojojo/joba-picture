92a6c8926c935ec36cbad2271beabb1940fc1b4c
yikuzojojo <1724078638@qq.com>
2025-04-24 15:29:58 +0800

    5.空间模块开发

---
+++ src/main/java/com/jojo/jobapicturebackend/service/SpaceService.java
@@ -0,0 +1,13 @@
+package com.jojo.jobapicturebackend.service;
+
+import com.jojo.jobapicturebackend.model.entity.Space;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 17240
+* @description 针对表【space(空间)】的数据库操作Service
+* @createDate 2025-04-24 15:26:29
+*/
+public interface SpaceService extends IService<Space> {
+
+}
---
+++ src/main/java/com/jojo/jobapicturebackend/service/impl/SpaceServiceImpl.java
@@ -0,0 +1,22 @@
+package com.jojo.jobapicturebackend.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jojo.jobapicturebackend.model.entity.Space;
+import com.jojo.jobapicturebackend.service.SpaceService;
+import com.jojo.jobapicturebackend.mapper.SpaceMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author 17240
+* @description 针对表【space(空间)】的数据库操作Service实现
+* @createDate 2025-04-24 15:26:29
+*/
+@Service
+public class SpaceServiceImpl extends ServiceImpl<SpaceMapper, Space>
+    implements SpaceService{
+
+}
+
+
+
+
